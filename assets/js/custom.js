99dff956661fecd862c86939dfe694773bbca643
kinghei.chick <khchick@gmail.com>
2018-05-24 10:14:16 +0800

    Added comments

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+// USER TRIGGER EFFECTS
+
 $(document).ready(function(){
     $('#nav-icon').click(function(){
       $(this).toggleClass('animate-icon');
@@ -22,16 +24,7 @@ $(document).ready(function(){
     
 });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-document.querySelector(this.getAttribute('href')).scrollIntoView({
-    behavior: 'smooth'
-        });
-    });
-});
-
+// ANIMATE ON SCROLL
 var $animation_elements = $('.animation-element');
 var $window = $(window);
 
@@ -46,7 +39,6 @@ function check_if_in_view() {
     var element_top_position = $element.offset().top;
     var element_bottom_position = (element_top_position + element_height);
  
-    //check to see if this current container is within viewport
     if ((element_bottom_position >= window_top_position) &&
         (element_top_position <= window_bottom_position)) {
       $element.addClass('in-view');
@@ -58,6 +50,19 @@ function check_if_in_view() {
 
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
+
+// SMOOTH SCROLLING
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+        });
+    });
+});
+
+
 
 
 
